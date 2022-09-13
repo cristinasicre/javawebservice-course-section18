dff724a75ed4259f2abff0e63b2b2c19be46e26a
Cristina Sicre Flores <lasicrita@gmail.com>
2022-09-13 10:50:35 +0200

    Initial commit

---
+++ src/main/java/com/sicre/restwscourse1/RestwsCourse1Application.java
@@ -0,0 +1,13 @@
+package com.sicre.restwscourse1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwsCourse1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestwsCourse1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sicre/restwscourse1/RestwsCourse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicre.restwscourse1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwsCourse1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
